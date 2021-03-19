73761bd1ad6e1df04023aed6c6a8436518518b93
Edwin Pile <69055818+EdwinPilePrazeres@users.noreply.github.com>
2021-03-18 20:37:03 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import Weather from './components/weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
 export default function App() {
+
+
   
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
@@ -29,7 +32,11 @@ export default function App() {
       {(typeof data.main != 'undefined') ? (
         <Weather weatherData={data}/>
       ): (
-        <div></div>
+        <div>
+          <Dimmer active>
+            <Loader>Loading...</Loader>
+          </Dimmer>
+        </div>
       )}
     </div>
   );
